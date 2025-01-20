12449d3aff7488f8ca49d45a822309604516b660
BugHunter-X <askzenai@gmail.com>
2025-01-20 16:04:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+```javascript
+import { useRouter } from 'next/router';
+
+function MyComponent() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/another-page');
+  };
+
+  return (
+    <button onClick={handleClick}>Go to Another Page</button>
+  );
+}
+```
